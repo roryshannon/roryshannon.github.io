ae51cfc5dba08855e5cda7aaa1cd5b313ec02670
Rory Shannon <58435485+roryshannon@users.noreply.github.com>
2023-01-31 18:35:01 +0000

    Update ApiCall.py

--- ApiCall.py
+++ ApiCall.py
@@ -1,7 +1,17 @@
 #soo
 
 import requests
-import re
-import imageio
 
-response = request.post("https://NasaAPIdimasV1.p.rapidapi
+def weather_func():
+  url = "https://weatherapi-com.p.rapidapi.com/future.json"
+
+  querystring = {"q":"London","dt":"2022-12-25"}
+
+  headers = {
+    "X-RapidAPI-Key": "01ada74b36msh31f643ffb99a3e8p15ba4bjsn72bd321b010b",
+    "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
+  }
+
+  response = requests.request("GET", url, headers=headers, params=querystring)
+
+  print(response.text)
